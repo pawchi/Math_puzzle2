f3d5505f8803a8ad486f104a56ead2a8122504db
pawchi <pawchi@o2.pl>
2018-05-08 16:24:18 +0200

    Initial commit

---
+++ app/src/main/java/org/chilon/math_puzzle2/activity_3.java
@@ -0,0 +1,13 @@
+package org.chilon.math_puzzle2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+    }
+}
